21fc6c895b78c0bcf39b52dda2deb2cb27bbac71
Lucas <lucasmartinscaet@hotmail.com>
2023-02-18 22:06:49 -0300

    feat: search field para cpfs

--- app/customers/api/viewsets.py
+++ app/customers/api/viewsets.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter
 from customers.api.serializers import CustomerSerializer
 from customers.models import Customer
-from rest_framework.pagination import PageNumberPagination
 
 
 
@@ -10,6 +11,14 @@ class ShortPagination(PageNumberPagination):
 
 
 class CustomerViewset(ModelViewSet):
+    
+    #FIXME Além do cpf ser pesquisado sem os
+    # pontos, também ser pesquisado com 
+    # os pontos. Trazer resultados
+    # para ambos as pesquisas. 
+    
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
     pagination_class = ShortPagination
+    filter_backends = [SearchFilter]
+    search_fields = ["cpf"]
