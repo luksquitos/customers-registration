b29b695f7bee013fc511494ec0bf0132f95608fa
Lucas <lucasmartinscaet@hotmail.com>
2023-02-18 21:27:19 -0300

    feat: paginação no endpoint de customers

--- app/customers/api/viewsets.py
+++ app/customers/api/viewsets.py
@@ -1,8 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from customers.api.serializers import CustomerSerializer
 from customers.models import Customer
+from rest_framework.pagination import PageNumberPagination
+
+
+
+class ShortPagination(PageNumberPagination):
+    page_size = 10
 
 
 class CustomerViewset(ModelViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
+    pagination_class = ShortPagination
