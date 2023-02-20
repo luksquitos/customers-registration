9da8e60440bd6152ccc744323e8926759c70a705
Lucas <lucasmartinscaet@hotmail.com>
2023-02-19 22:16:03 -0300

    feat: Criando branch para testes da função e remoção do arquivo views

--- app/customers/tests.py
+++ app/customers/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+
+# These tests are just for testing 
+# the 'is_cpf_valid' function present
+# in utils.py
+
+# Para executar esse arquivo isoladamente
+# é python customers/tests.py 
+# pelo bash do container
+
+if __name__ == '__main__':
+    unittest.main
